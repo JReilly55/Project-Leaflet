c6fac982718f30fd1d753438f9dd82ef9261d6dc
JReilly55 <98059081+JReilly55@users.noreply.github.com>
2022-02-22 21:58:49 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 var mymap = L.map('mapid').setView([37.0902, -95.7129], 4);
 
-var OpenStreetMap_Mapnik = L.tileLayer('https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png', {
+var Stamen_TonerLite = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
 	maxZoom: 19,
   minZoom: 4,
-	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+  subdomains: 'abcd',
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  ext: "png"
 }).addTo(mymap);
 
 var fire = $.getJSON("https://raw.githubusercontent.com/JReilly55/Project-Leaflet/main/USA_Current_Wildfires.geojson",function(data){
